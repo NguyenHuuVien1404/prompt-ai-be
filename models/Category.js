003ac6d95b9c03ecff1bf53488d7870ecbbdd9e8
haiduong2703 <duong270302@gmail.com>
2025-02-20 09:50:46 +0700

    duong commit models section

--- models/Category.js
+++ models/Category.js
@@ -4,6 +4,7 @@ const sequelize = require("../config/database");
 const Category = sequelize.define("Category", {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: { type: DataTypes.STRING(100), allowNull: false },
+    image: { type: DataTypes.STRING(500), allowNull: false },
     description: { type: DataTypes.TEXT, allowNull: true },
     section_id: { type: DataTypes.INTEGER, allowNull: false, references: { model: Section, key: "id" } },
     created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
