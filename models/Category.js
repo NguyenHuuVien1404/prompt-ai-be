afc0dfce42f544cb865122e25773bea424d83d1b
AnhN-V <vietanhgnuyenit201002@gmail.com>
2025-02-20 09:54:02 +0700

    add them truong image

--- models/Category.js
+++ models/Category.js
@@ -5,6 +5,7 @@ const Category = sequelize.define("Category", {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: { type: DataTypes.STRING(100), allowNull: false },
     description: { type: DataTypes.TEXT, allowNull: true },
+    image: { type: DataTypes.STRING(500), allowNull: false },
     created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
     updated_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
 }, {
