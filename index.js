9c130cd46573f1f0af3f7d31a6e1f3a9a75b9953
AnhN-V <vietanhgnuyenit201002@gmail.com>
2025-02-21 10:41:11 +0700

    Add api for main prompts page

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const userRoutes = require('./routes/userRoutes');
 const promptRoutes = require('./routes/promptRoutes');
 const categoryRoutes = require("./routes/categoryRoutes");
 const contactRoutes = require("./routes/contactRoutes");
+const sectionRoutes = require("./routes/sectionRoutes");
 
 dotenv.config();
 const app = express();
@@ -20,5 +21,6 @@ app.use('/api/users', userRoutes);
 app.use('/api/prompts', promptRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/contact", contactRoutes);
+app.use("/api/sections", sectionRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ routes/sectionRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const Section = require("../models/Section")
+
+// Lấy danh sách section
+router.get("/", async (req, res) => {
+    try {
+        const sections = await Section.findAll();
+        res.json(sections);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+module.exports = router;
