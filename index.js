4be394142e60f3c04cae4e14181a8a75031e535a
Richard <nguyenhuuvien2065@gmail.com>
2025-05-06 19:07:54 +0700

    test

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ app.use("/api/payment", paymentRouters);
 app.use("/api/referral", referralRoutes);
 app.use("/api/chat", chatGPTRoutes);
 app.use("/api/history", historyRoutes);
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
