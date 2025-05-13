3b007e872053afbf117ca622916bf7420b3b7623
Richard <nguyenhuuvien2065@gmail.com>
2025-05-13 20:22:37 +0700

    fix-cron

--- cronJob.js
+++ cronJob.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const { User } = require('./models');
 
 // Lập lịch cron để cập nhật count_promt mỗi đêm lúc 0h Chủ Nhật hàng tuần
-cron.schedule('0 0 * * 0', async () => {
+cron.schedule('0 0 1 * *    ', async () => {
     try {
         // Cập nhật count_promt của tất cả user về 5
         await User.update(
