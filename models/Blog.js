68cab98877275dfff081887e25c6f8628d178060
nhân <hoang94nhan@gmail.com>
2025-08-14 20:29:07 +0700

    refactor: improve code formatting and structure in Blog model definition for better readability and maintainability

--- models/Blog.js
+++ models/Blog.js
@@ -4,53 +4,52 @@ const User = require("./User"); // Import User để làm khóa ngoại
 const BlogCategory = require("./BlogCategory"); // Import BlogCategory để làm khóa ngoại
 
 const Blog = sequelize.define(
-    "Blog",
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        title: {
-            type: DataTypes.STRING(255),
-            allowNull: false,
-        },
-        content: {
-            type: DataTypes.TEXT("long"),
-            allowNull: false,
-        },
-
-
-        category_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: BlogCategory,
-                key: "id",
-            },
-            onDelete: "CASCADE",
-        },
-        published_at: {
-            type: DataTypes.DATE,
-            allowNull: true,
-        },
-
-        meta_description: {
-            type: DataTypes.STRING(255),
-        },
-        featured_image: {
-            type: DataTypes.TEXT,
-        },
+  "Blog",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-        tableName: "blogs",
-        timestamps: true,
-        createdAt: "created_at",
-        updatedAt: "updated_at",
-    }
+    title: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT("long"),
+      allowNull: false,
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: BlogCategory,
+        key: "id",
+      },
+      onDelete: "CASCADE",
+    },
+    published_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+
+    meta_description: {
+      type: DataTypes.STRING(255),
+    },
+    featured_image: {
+      type: DataTypes.TEXT,
+    },
+  },
+  {
+    tableName: "blogs",
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+  }
 );
 Blog.belongsTo(BlogCategory, {
-    foreignKey: "category_id",
-    as: "category", // Định danh alias để dùng trong truy vấn
+  foreignKey: "category_id",
+  as: "category", // Định danh alias để dùng trong truy vấn
 });
 module.exports = Blog;
